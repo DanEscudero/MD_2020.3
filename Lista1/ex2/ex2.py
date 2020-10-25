c45dbc31dbc68f5391ad68f29c1e489f63857ef9
Daniel Escudero <daniel.escudero05@gmail.com>
2020-10-25 12:30:44 -0300

    ex2

--- Lista1/ex2/ex2.py
+++ Lista1/ex2/ex2.py
@@ -24,42 +24,45 @@ def prob(className, data):
     total = len(data)
     return c/total
 
-def qda_g_k(k, X):
-    i = k-1
-    X = np.array(X)
-    T1 = - (1/2) * transpose(X) * inverse(covars[i]) * X
-    T2 = + transpose(avgs[i]) * inverse(covars[i]) * X
-    T3 = - (1/2) * transpose(avgs[i]) * inverse(covars[i]) * avgs[i]
-    T4 = - (1/2) * math.log(determinant(covars[i]))
-    T5 = + math.log(prob(classes[i], data))
-    return T1 + T2 + T3 + T4 + T5
-
+# k eh o indice da classe, de 1 a 4
 def lda_g_k(k, X):
-    cov = xs.cov()
-    i = k-1
+    i = k - 1
     X = np.array(X)
-    T1 = transpose(X) * cov * avgs[i]
-    T2 = - (1/2) * transpose(avgs[i]) * inverse(cov)
+    T1 = transpose(X).dot(covariance).dot(avgs[i])
+    T2 = - (1/2) * transpose(avgs[i]).dot(inverse(covariance)).dot(avgs[i])
     T3 = math.log(prob(classes[i], data))
+
     return T1 + T2 + T3
 
-def plot(discriminant):
-    xs = np.arange(-10, 15, 0.25)
-    ys = np.arange(-2, 10, 0.25)
+# k eh o indice da classe, de 1 a 4
+def qda_g_k(k, X):
+    i = k - 1
+    X = np.array(X)
+    T1 = - (1/2) * transpose(X).dot(inverse(covars[i])).dot(X)
+    T2 = + transpose(avgs[i]).dot(inverse(covars[i])).dot(X)
+    T3 = - (1/2) * transpose(avgs[i]).dot(inverse(covars[i])).dot(avgs[i])
+    T4 = - (1/2) * math.log(determinant(covars[i]))
+    T5 = + math.log(prob(classes[i], data))
+
+    return T1 + T2 + T3 + T4 + T5
+
+# Plota os pontos previstos pela funcao discriminante (lda ou qda)
+def plot(discriminant, ax):
+    xs = np.arange(-10, 15, 0.2)
+    ys = np.arange(-2, 10, 0.2)
     predicted = []
     for x in xs:
         for y in ys:
             d = dict()
-            d['c1'] = -determinant(discriminant(1, [x,y]))
-            d['c2'] = -determinant(discriminant(2, [x,y]))
-            d['c3'] = -determinant(discriminant(3, [x,y]))
-            d['c4'] = -determinant(discriminant(4, [x,y]))
-            _class = max(d, key=d.get)
-            predicted.append([x,y,_class])
+            d['c1'] = discriminant(1, [x,y])
+            d['c2'] = discriminant(2, [x,y])
+            d['c3'] = discriminant(3, [x,y])
+            d['c4'] = discriminant(4, [x,y])
+            className = max(d, key=d.get)
+            predicted.append([x, y, className])
     
     predicted = pd.DataFrame(data=predicted, columns=['x','y','c'])
-    sns.scatterplot(data=predicted, x='x', y='y', hue='c')
-    plt.show()
+    sns.scatterplot(data=predicted, x='x', y='y', hue='c', ax=ax)
 
 
 if __name__ == "__main__":
@@ -78,23 +81,13 @@ if __name__ == "__main__":
     
     xs = data[['x', 'y']]
     ys = data['c']
+    covariance = xs.cov()
     
     xis = list(map(lambda classValue: data[data['c'] == classValue][attributes], classes))
     avgs, covars = getAvgs(xis), getCovars(xis)
 
-    X = [4, 4]
-
-    print('QDA')
-    # print(determinant(qda_g_k(1, X)))
-    # print(determinant(qda_g_k(2, X)))
-    # print(determinant(qda_g_k(3, X)))
-    # print(determinant(qda_g_k(4, X)))
-
-    plot(qda_g_k)
-
-    # print('____')
-    # print('LDA')
-    # print(lda_g_k(1, X))
-    # print(lda_g_k(2, X))
-    # print(lda_g_k(3, X))
-    # print(lda_g_k(4, X))
+    # Fig 2
+    fig, axs = plt.subplots(ncols=2)
+    plot(lda_g_k, axs[0])
+    plot(qda_g_k, axs[1])
+    plt.show()
